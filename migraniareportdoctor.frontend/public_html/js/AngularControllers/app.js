61477c918d339aba712adab58b9e673184c8192a
Jessica Chaparro <ja.chaparro914@uniandes.edu.co>
2015-05-20 17:28:23 -0500

    Intentando con dolor modificar las persistence unit que no me quieren. Ah, y empezando a arreglar el layout.

--- migraniareportdoctor.frontend/public_html/js/AngularControllers/app.js
+++ migraniareportdoctor.frontend/public_html/js/AngularControllers/app.js
@@ -3,5 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+(function(){var migraniaReport= angular.module('migraniaReport',[]);
 
+migraniaReport.directive('toolbar', function(){
+        return{
+            restrict:'E',
+            templateUrl: 'partials/toolbar.html',
+            controller:function(){
+                this.tab=0;
+                this.selectTab=function(setTab){
+                    this.tab=setTab;
+                };
+                this.isSelected=function(tabParam){
+                    return this.tab===tabParam;
+                };
+            },
+            controllerAs:'toolbar'
+        };
+    });
+
+migraniaReport.directive('pacientesInfo', function(){
+        return{
+            restrict:'E',
+            templateUrl:'partials/pacientes-info.html',
+            controller: ['$http',function($http){
+                var self=this;
+                self.pacientes=[];
+                    $http.get('URL del Servicio').success(function(data){
+                        self.pacientes=data;
+                    });
+            }],
+            controllerAs:'getPacientes'
+        };
+    });
+})();
 
