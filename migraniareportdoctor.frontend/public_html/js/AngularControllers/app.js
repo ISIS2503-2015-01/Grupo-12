01a8cb512ce5ee299c7b13d1f62e3cd42babd2a0
Jessica Chaparro <ja.chaparro914@uniandes.edu.co>
2015-05-21 12:37:13 -0500

    Intentando con dolor modificar las persistence unit que no me quieren. Ah, y el layout está casi listo. D= D= D=

--- migraniareportdoctor.frontend/public_html/js/AngularControllers/app.js
+++ migraniareportdoctor.frontend/public_html/js/AngularControllers/app.js
@@ -5,27 +5,10 @@
  */
 (function(){var migraniaReport= angular.module('migraniaReport',[]);
 
-migraniaReport.directive('toolbar', function(){
-        return{
-            restrict:'E',
-            templateUrl: 'partials/toolbar.html',
-            controller:function(){
-                this.tab=0;
-                this.selectTab=function(setTab){
-                    this.tab=setTab;
-                };
-                this.isSelected=function(tabParam){
-                    return this.tab===tabParam;
-                };
-            },
-            controllerAs:'toolbar'
-        };
-    });
-
 migraniaReport.directive('pacientesInfo', function(){
         return{
             restrict:'E',
-            templateUrl:'partials/pacientes-info.html',
+            templateUrl:'pages/pacientes-info.html',
             controller: ['$http',function($http){
                 var self=this;
                 self.pacientes=[];
